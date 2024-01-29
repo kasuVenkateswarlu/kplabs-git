d1f776299d2a5f9e11a28e34650dc48f722923b3
Kasu_Venkateswarlu <87533908+kasuVenkateswarlu@users.noreply.github.com>
2024-01-29 10:59:29 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -2,7 +2,8 @@ provider "aws" {
   region     = "us-east-1"
 }
 
-resource "aws_instance" "myec2" {
+/* resource "aws_instance" "myec2" {
    ami = "ami-0a3c3a20c09d6f377"
    instance_type = "t2.micro"
 }
+*/
